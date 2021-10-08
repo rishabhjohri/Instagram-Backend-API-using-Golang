1add3e27b4b9d56fca3fa9bcfb78b63c8bc472e1
rishabhjohri <54755134+rishabhjohri@users.noreply.github.com>
2021-10-08 20:22:57 +0530

    Create test0.go

---
+++ testcase/test0.go
@@ -0,0 +1,26 @@
+// author : Rishabh Johri
+// description : Test case 0 (init file)
+package main
+
+import (
+	"fmt"
+	"github.com/rishabhjohri/Instagram-Backend-API-using-Golang/API"
+	"os"
+)
+
+func main() {
+
+	client := instagram.NewClient(nil)
+	client.ClientID = "8f2c0ad697ea4094beb2b1753b7cde9c"
+
+	media, next, err := client.Media.Popular()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+	}
+	for _, m := range media {
+		fmt.Printf("ID: %v, Type: %v\n", m.ID, m.Type)
+	}
+	if next.NextURL != "" {
+		fmt.Println("Next URL", next.NextURL)
+	}
+}
